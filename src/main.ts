445189800ed04a3cbffd0f1f1c98d62ca02cf683
leogm360 <leogm360@gmail.com>
2022-08-30 21:29:15 -0300

    :bug: hotfix: correct cors policy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from 'cors';
 
 const port = Number(process.env.PORT) || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: '*' },
+    logger: false,
+  });
+
   await app.listen(port);
 }
 
 bootstrap();
-
-// { logger: false }
